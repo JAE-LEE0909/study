77fc4b869cc8277ab4313b5a941b7cd2f2effbf6
JAE-LEE0909 <kkao456178@gmail.com>
2025-11-25 21:15:32 +0900

    수정된코드 업로드
    업로드

--- example1.java
+++ example1.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class example1{
     public static void main(String[] args) {
-        vector v = new vector(5);
+        Vector<Object> v = new Vector<>(5);
         v.add("1");
         v.add("2");
         v.add("3");
         v.add("4");
-        print(v);
 
         v.trimToSize();
         System.out.println("======After trimToSize()======");
 
-        print(v)
 
         v.setSize(7);
-        System.out.println("======After setSize(7)======")
+        System.out.println("======After setSize(7)======");
+
+        
 
     }
+
 }
